15b31dc21fe23d33cde180b56949500f59d3439f
mohitnivasjs <mohitnivas05@gmail.com>
2026-07-09 09:45:55 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/263-UglyNumber.java
@@ -0,0 +1,16 @@
+// Last updated: 09/07/2026, 09:45:55
+public class Solution {
+    public boolean isUgly(int n) {
+        if (n <= 0) return false;
+        while (n % 2 == 0) n /= 2;
+        while (n % 3 == 0) n /= 3;
+        while (n % 5 == 0) n /= 5;
+        return n == 1;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int n = 14;
+        System.out.println(sol.isUgly(n));
+    }
+}
