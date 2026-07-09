62969c5b98ea59da6a21b84d824661538e2d7902
mohitnivasjs <mohitnivas05@gmail.com>
2026-07-09 09:46:02 +0530

    Create 204-CountPrimes

---
+++ 204-CountPrimes/204-CountPrimes.java
@@ -0,0 +1,25 @@
+// Last updated: 09/07/2026, 09:46:02
+class Solution{
+    public int countPrimes(int n){
+        int count = 0;
+        if(n<=2)
+            return count;
+        int[] arr = new int[n];
+        for(int i=2;i<n;i++){
+            arr[i] = 1;
+        }
+        for(int i=2;i*i<n;i++){
+            if (arr[i]==1){
+                for (int j=i*i;j<n;j+=i){
+                    arr[j]=0;
+                }
+            }
+        }
+        for(int i=2;i<n;i++){
+            if(arr[i]==1){
+                count++;
+            }
+        }
+        return count;
+    }
+}
