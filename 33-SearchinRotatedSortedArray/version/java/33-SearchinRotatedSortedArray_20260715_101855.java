c253a142c449b387db5db50f5aed6b4d2aeb1d69
mohitnivasjs <mohitnivas05@gmail.com>
2026-07-15 10:18:55 +0530

    Create 33-SearchinRotatedSortedArray

---
+++ 33-SearchinRotatedSortedArray/version/java/33-SearchinRotatedSortedArray_20260715_101855.java
@@ -0,0 +1,11 @@
+// Last updated: 15/07/2026, 10:18:55
+1class Solution {
+2    public int search(int[] nums,int target) {
+3        for(int i=0;i<nums.length;i++){
+4            if(nums[i]==target){
+5                return i;
+6            }
+7        }
+8        return -1;
+9    }
+10}
